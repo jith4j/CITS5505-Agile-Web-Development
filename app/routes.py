46c9b85a2c51dc6b23055d156d7553c334bf36d8
Jith <jith4j@gmail.com>
2024-05-09 00:34:24 +0800

    Implemented base.html template with navbar: Created a reusable template named base.html containing a navbar for universal use across template pages. Added a home page featuring a welcome message, previously housed in the index page. Updated page styling for improved aesthetics.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,9 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/index')
 def index():
     return render_template('index.html')
