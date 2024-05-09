2e1ea69d97a8ecdfe2a0371598843984dccea6ed
BhargavaKolli08 <bhargavasaigopal.kolli@outlook.com>
2024-05-09 17:20:04 +0800

    Created Login and Register html files +Added forms functionalty in forms.py. +Added html files for login and registration,  which uses the forms functionality defined in  forms.py. +Modified routes.py, base.html.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +14,17 @@ def success():
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('register.html', title='Register', form=form)
