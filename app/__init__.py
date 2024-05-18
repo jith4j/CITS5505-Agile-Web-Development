d07b7c32be030a108f161bebfcf95265f5438cf2
kureacoz-ks <kuriakoseksaju@gmail.com>
2024-05-19 05:36:36 +0800

    create_app function and test upgrade
    create_app function integrated into init app which creates an
    flask app instance and initialize the app using the config file
    passed in as argument. The function intializes the db, migrates to
    the latest version and login form. Finally a blueprint is registered
    before the app instance is returned.
    
    The function is used within test.py to call a app instance with
    a non-persistent db for testing

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 login.login_view = 'login'
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init(app)
+
+    from app.blueprints import main
+    app.register_blueprint(main)
+
+    return app
 
 from app import routes,models
